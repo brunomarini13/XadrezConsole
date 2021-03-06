06d36bde5ca131e048a97e9744ffe53e75921ba2
Bruno Marini <brunook13@outlook.com>
2021-03-06 15:34:48 -0300

    test

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,9 +7,7 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao P = new Posicao(3, 4);
 
-            Console.WriteLine(P);
         }
     }
 }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace xadrez_console.tabuleiro
+﻿namespace tabuleiro
 {
     class Tabuleiro
     {
+        public int Linhas { get; set; }
+
+        public int Colunas { get; set; }
+
+        private Peca[,] Pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[linhas, colunas];
+        }
     }
 }
